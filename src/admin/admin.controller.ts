dba90c79aa94e3f04175e7638e762db98ec6c351
MikelNoppo <mikelnoppo03@gmail.com>
2025-08-09 22:44:14 +0800

    feat(growth): add authentication and role guards to growth record endpoint

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,10 +1,7 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AdminService } from './admin.service';
-import { AuthGuard } from '@nestjs/passport';
-import { RolesGuard } from 'src/auth/roles/roles.guard';
 import { Prisma } from '@prisma/client';
 
-@UseGuards(AuthGuard('jwt'), RolesGuard)
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
--- src/growth/growth.controller.ts
+++ src/growth/growth.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Post, Param, ParseIntPipe, Body, ValidationPipe} from '@nestjs/common';
+import { Controller, Post, Param, ParseIntPipe, Body, ValidationPipe, UseGuards} from '@nestjs/common';
 import { GrowthService } from './growth.service';
 import { CreateGrowthRecordDto } from './dto/CreateGrowthRecordDto';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/roles/roles.guard';
+import { Roles } from 'src/auth/roles/roles.decorator';
 
 @Controller('growth')
 export class GrowthController {
   constructor(private readonly growthService: GrowthService) {}
 
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('PEGAWAI', 'DOKTER', 'ADMIN')
   @Post(':childId/growth-records')
   async addGrowthRecord(
     @Param('childId', ParseIntPipe) childId: number,
